b40dbd6bca062d875c57768cd928c7f785675944
leehi0210 <leehi0210@gmail.com>
2020-03-05 02:15:21 +0900

    add ft_strtrim.c

---
+++ ft_strtrim.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daelee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/04 20:52:40 by daelee            #+#    #+#             */
+/*   Updated: 2020/03/05 02:13:29 by daelee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int     ft_getstart(const char *s1, const char *set)
+{
+    int i;
+    char *temp;
+
+    i = 0;
+    while (s1[i])
+    {
+        if (ft_strchr(set, s1[i]) == 0)
+            break ;
+        i++;
+    }
+    return (i);
+}
+
+int     ft_getend(const char *s1, const char *set)
+{
+    int len;
+
+    len = ft_strlen(s1);
+    while (len)
+    {
+        if (ft_strchr(set, s1[len - 1]) == 0)
+            break ;
+        len--;
+    }
+    return (len);
+}
+
+char    *ft_strtrim(char const *s1, char const *set)
+{ 
+    int start;
+    int end;
+    char *newstr;
+
+    if (!s1)
+        return (ft_strdup(""));
+    start = ft_getstart(s1, set);
+    end = ft_getend(s1, set);
+    newstr = (char *)malloc(sizeof(char) * (end - start + 1) + 1);
+    if (newstr == NULL)
+        return (NULL);
+    ft_strlcpy(newstr, s1 + start, end - start + 1);
+    return(newstr);
+}
+
+int     main()
+{
+    char *s1 = "  ABCCBA  "; 
+    char *set1 = "";
+    //résultat : "ABCCBA";
+    char *s2 = "ACCBACBA"; 
+    char *set2 = "AB";
+    //résultat : "CCBAC";
+    char *s3 = "Hello World!";  
+    char *set3 = "Hlde";
+    //résultat : "o World!"
+    printf("1..%s\n2..%s\n3..%s\n", ft_strtrim(s1, set1), ft_strtrim(s2, set2), ft_strtrim(s3, set3));
+    return (0);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: daelee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 17:19:20 by daelee            #+#    #+#             */
-/*   Updated: 2020/03/02 14:39:31 by daelee           ###   ########.fr       */
+/*   Updated: 2020/03/05 02:14:56 by daelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,5 +42,7 @@ int	    ft_tolower(int c);
 void    *ft_calloc(size_t nmemb, size_t size);
 char    *ft_strdup(const char *str);
 char    *ft_substr(char const *s, unsigned int start, size_t len);
+char    *ft_strjoin(char const *s1, char const *s2);
+char    *ft_strtrim(char const *s1, char const *set);
 
 #endif
