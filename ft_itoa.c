bc8f69a37417ff526d030f1430208abb4b2f0e19
leehi0210 <leehi0210@gmail.com>
2020-03-05 18:05:43 +0900

    add ft_itoa.c

---
+++ ft_itoa.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daelee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/05 17:51:44 by daelee            #+#    #+#             */
+/*   Updated: 2020/03/05 18:00:21 by daelee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long int		ft_abs(long int nbr)
+{
+	return ((nbr < 0) ? -nbr : nbr);
+}
+
+int				ft_len(long int nbr)
+{
+	int		len;
+
+	len = (nbr <= 0) ? 1 : 0;
+	while (nbr != 0)
+	{
+		nbr = nbr / 10;
+		len++;
+	}
+	return (len);
+}
+
+char			*ft_itoa(int n)
+{
+	int			len;
+	int			sign;
+	char		*c;
+
+	sign = (n < 0) ? -1 : 1;
+	len = ft_len(n);
+	c = (char *)malloc(sizeof(char) * len + 1);
+	c[len] = '\0';
+	len--;
+	while (len >= 0)
+	{
+		c[len] = '0' + ft_abs(n % 10);
+		n = ft_abs(n / 10);
+		len--;
+	}
+	if (sign == -1)
+		c[0] = '-';
+	return (c);
+}
+
+int     main(void)
+{
+    int n = 456;
+    printf("answer : %s\n", ft_itoa(n));
+    return (0);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: daelee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 17:19:20 by daelee            #+#    #+#             */
-/*   Updated: 2020/03/05 02:14:56 by daelee           ###   ########.fr       */
+/*   Updated: 2020/03/05 18:05:22 by daelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,5 +44,7 @@ char    *ft_strdup(const char *str);
 char    *ft_substr(char const *s, unsigned int start, size_t len);
 char    *ft_strjoin(char const *s1, char const *s2);
 char    *ft_strtrim(char const *s1, char const *set);
+char				**ft_split(char const *s, char c);
+char			*ft_itoa(int n);
 
 #endif
