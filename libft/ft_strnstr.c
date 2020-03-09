4ac09bff8fdfe9f6e8b324653ae38b1ae4fc8b88
leehi0210 <leehi0210@gmail.com>
2020-03-10 01:36:49 +0900

    modified ft_strnstr.c

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: daelee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 23:05:09 by daelee            #+#    #+#             */
-/*   Updated: 2020/03/09 20:27:59 by daelee           ###   ########.fr       */
+/*   Updated: 2020/03/10 01:35:49 by daelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char        *ft_strnstr(const char *big, const char *little, size_t len)
 {
     size_t  l_len;
     size_t  b_len;
-    size_t  len2;
+    size_t  size;
 
     if (*little == '\0')
         return ((char *)big);
@@ -24,10 +24,10 @@ char        *ft_strnstr(const char *big, const char *little, size_t len)
     b_len = ft_strlen(big);
     if (b_len < l_len || len < l_len)
 		return (0);
-	len2 = b_len > len ? len : b_len;
-    while (len2--)
+	size = b_len > len ? len : b_len;
+    while (size-- >= l_len)
     {
-        if (ft_memcmp(big, little, (l_len - 1)) == 0)
+        if (ft_memcmp(big, little, l_len) == 0)
             return ((char *)big);
         big++;
     }
