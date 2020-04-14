f3f7b048a1eb0126a6315d496d3be38027ae80de
leehi0210 <leehi0210@gmail.com>
2020-04-14 10:37:22 +0900

    fix abort error

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: daelee <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 21:04:42 by daelee            #+#    #+#             */
-/*   Updated: 2020/04/10 11:29:07 by daelee           ###   ########.fr       */
+/*   Updated: 2020/04/14 10:35:44 by daelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,16 @@ size_t		ft_strlcat(char *dest, const char *src, size_t dstsize)
 	dest_len = ft_strlen(dest);
 	src_len = ft_strlen(src);
 	i = 0;
-	while (src[i] != '\0' && dest_len + 1 + i < dstsize)
+	if (dstsize < dest_len + 1)
+		return (dstsize + src_len);
+	if (dstsize > dest_len + 1)
 	{
-		dest[dest_len + i] = src[i];
-		i++;
+		while (src[i] != '\0' && dest_len + 1 + i < dstsize)
+		{
+			dest[dest_len + i] = src[i];
+			i++;
+		}
 	}
 	dest[dest_len + i] = '\0';
-	if (dstsize < dest_len)
-		return (dstsize + src_len);
 	return (dest_len + src_len);
 }
